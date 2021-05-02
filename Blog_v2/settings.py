beb4d5743f2b38399abfa2dfa81fb3abd808280d
Artyom <virytinart1@gmail.com>
2021-05-02 21:12:28 +0300

    Permissions

--- Blog_v2/settings.py
+++ Blog_v2/settings.py
@@ -141,7 +141,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chessaggregator@gmail.com'
-EMAIL_HOST_PASSWORD = 'Vfheczz55'
-DEFAULT_FROM_EMAIL = 'chessaggregator@gmail.com'
-
